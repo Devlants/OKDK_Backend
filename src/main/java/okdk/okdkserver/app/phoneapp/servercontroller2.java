80d496699b1404215328e3cb42dc404711390a91
JaeHun Leem <80560040+walloonam@users.noreply.github.com>
2023-04-15 02:22:44 +0900

    mobile application server333

--- src/main/java/okdk/okdkserver/app/phoneapp/servercontroller2.java
+++ src/main/java/okdk/okdkserver/app/phoneapp/servercontroller2.java
@@ -3,6 +3,7 @@ package okdk.okdkserver.app.phoneapp;
 import lombok.RequiredArgsConstructor;
 import okdk.okdkserver.app.keyosk.PythonSocet.PythonSocketForRegister;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class servercontroller2{
 
+    @CrossOrigin
     @PostMapping(value = "/server/register",consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public String registerImage(@RequestParam MultipartFile imgFile) throws IOException {
         System.out.println(imgFile);
